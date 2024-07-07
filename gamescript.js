68bd101b34db6dd55e2240e5b629fad8e89d1c4e
mugisachantal <mjojassen94@gmail.com>
2024-07-07 16:58:38 +0300

    i have added a new files

--- gamescript.js
+++ gamescript.js
@@ -6,7 +6,7 @@ let xp =0;
 let health = 100;
 let gold= 120;
 let currentweapon = 0;
-let fighting ;
+let fighting;
 let monsterhealth;
 let inventory = ['stick'];
 let weapons = [
@@ -29,7 +29,8 @@ let weapons = [
 ];
 
 
-const dev2 = document.getElementById('D2')
+const dev1 = document.getElementById('D1');
+const dev2 = document.getElementById('D2');
 const button1=document.getElementById('button1');
 const button2=document.getElementById('button2');
 const button3=document.getElementById('button3');
@@ -37,6 +38,12 @@ let xpText = document.getElementById('xptext');
 let goldText = document.getElementById('goldtext');
 let healthText = document.getElementById('healthtext');
 let text = document.getElementById('p');
+let reset = document.createElement("button");
+let textReset = document.createTextNode('RESET');
+reset.appendChild(textReset);
+dev1.appendChild(reset);
+reset.onclick = starting;
+
 button1.onclick =goToStore;
 
 function goToStore(){
@@ -61,6 +68,7 @@ function goTown(){
      button3.onclick =fightDragon;
 }
 
+
 function buyHealth(){
     if (gold >= 10){
     gold-=10;
@@ -68,18 +76,20 @@ function buyHealth(){
     goldText.innerHTML=gold;
     healthText.innerHTML =health;
 }
+
 else{
     alert("oops! you have insuficient  gold")
 }
 
 }
 
+
 function buyWeapon(){
     if (currentweapon < weapons.length-1){
         if (gold>=30){
             gold -= 30;
             goldText.innerText = gold;
-            currentweapon++;
+            ++currentweapon;
             inventory.push(weapons[currentweapon].name);
             text.innerHTML = "you <b>have bought a </b> "+ weapons[currentweapon].name + " and now you have " + inventory + "  in your weapon base ."
             }
@@ -102,31 +112,134 @@ function sellWeapon(){
     button3.innerText = "sell claw";
     let button4 = document.createElement('button');
     button4.innerText = "sell sword";
-    text.innerText = "sell stick for" + Stickprice + "sell dagger for" + daggerprice + "sell clawhammer for" + clawprice + "sell sword for" + swordprice + ".";
+    text.innerHTML = "sell stick for  "  + Stickprice + "<span style = 'color:yellow'>Gold</span>,  sell dagger for  " + daggerprice + "<span style = 'color:yellow'>Gold</span>,  sell clawhammer for  " + clawprice + "<span style = 'color:yellow'>Gold</span>,  sell sword for  " + swordprice + "<span style = 'color:yellow'>Gold</span>.";
     dev2.appendChild(button4);
     button1.onclick = sellStick;
-    button1.onclick = sellDagger;
-    button1.onclick = sellClawHammer;
-    button1.onclick = sellSword;
+    button2.onclick = sellDagger;
+    button3.onclick = sellClawHammer;
+    button4.onclick = sellSword;
+    
     }
     else{
         text.innerText="Do not sell your only weapon";
     }
-    
+    return button4;
 }
 
+function goCave(){
+    alert('mugisa')
+}
+
+
 function sellStick(){
-        gold += Stickprice;
-        goldText.innerText = gold;}
+    if(inventory.includes("stick")){
+
+    if(currentweapon>0){
+        
+            gold += Stickprice;
+            goldText.innerText = gold;
+            inventory.splice(inventory.indexOf("stick"),1);
+            text.innerText = 'You have sold your stick for 5 gold .THANK YOU FOR TRADING WITH US '
+            --currentweapon;
+        }
+       
+        else{
+            text.innerText="Do not sell your only weapon";
+        }
+    }
+    else{
+        text.innerText = "YOU DONT HAVE A STICK IN YOUR WEAPON BASE  ";
+    }
+    
+
+    }
 function sellDagger(){
-            gold += daggerpriceprice;
-            goldText.innerText = gold;}
+    if(inventory.includes("dagger")){
+
+    if(currentweapon>0){
+        
+            gold += daggerprice;
+            goldText.innerText = gold;
+            inventory.splice(inventory.indexOf("dagger"),1);
+            text.innerText = 'You have sold your dagger for 10 gold .THANK YOU FOR TRADING WITH US '
+            --currentweapon;
+        }
+        
+        else{
+            text.innerText=" Do not sell your only weapon";
+        }
+    }
+    else{
+        text.innerText = "YOU DONT HAVE A DAGGER IN YOUR WEAPON BASE  ";
+    }
+    }
 function sellClawHammer(){
-                gold += clawprice;
-                goldText.innerText = gold;}
+    if(inventory.includes("claw hammer")){
+    if(currentweapon>0){
+        
+
+            gold += clawprice;
+            goldText.innerText = gold;
+            inventory.splice(inventory.indexOf("claw hammer"),1);
+            text.innerText = 'You have sold your claw hammer for 15 gold .THANK YOU FOR TRADING WITH US '
+            --currentweapon;
+        }
+       
+        else{
+            text.innerText=" Do not sell your only weapon";
+        }
+    }
+    else{
+        text.innerText = "YOU DONT HAVE A CLAW HAMMER IN YOUR WEAPON BASE  ";
+    }
+   
+    }
 function sellSword(){
-                    gold += swordprice;
-                    goldText.innerText = gold;}
+    if(inventory.includes("sword")){
+    if(currentweapon){
+        
+
+            gold += swordprice;
+            goldText.innerText = gold;
+            inventory.splice(inventory.indexOf("sword"),1);
+            text.innerText = 'You have sold your SWORD for 20 gold .THANK YOU FOR TRADING WITH US '
+            --currentweapon;
+        }
+        else{
+            text.innerText=" Do not sell your only weapon";
+        }
+       
+    }
+    else{
+        text.innerText = "YOU DONT HAVE A SWORD IN YOUR WEAPON BASE  ";
+    }
+    }
+
+                    function starting()   {
+                        text.innerHTML = 'Wellcome to dragon repeller.you must defeat the dragon that is preventing people from leaving the town. You are in the town square.Where do you want to go?use the buttons above .'
+                    button1.innerHTML = 'Go to store'
+                    button2.innerHTML = "Go to cave"
+                    button3.innerHTML = "Fight dragon"
+                    xpText.innerHTML ="<strong>0</strong>"
+                    healthText.innerHTML = "<strong>100</strong>"
+                    goldText.innerHTML = "<strong>50</strong>"
+                    health = 100;
+                    xp = 0;
+                    gold = 120
+                    currentweapon = 0;
+                    inventory = ['stick'];
+                    button1.onclick = goToStore;
+                     button2.onclick =goCave;
+                     button3.onclick =fightDragon;
+                     
+                                                     }
+
+                      
+                    
+                    
+                    
+        
+                    
                                         
         
     
